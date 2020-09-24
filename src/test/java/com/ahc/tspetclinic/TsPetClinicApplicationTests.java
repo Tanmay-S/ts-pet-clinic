6e8ab61f3c1c990d006c703a1f121e1a66886afb
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-09-25 01:35:52 +0530

    Initial Project Add

---
+++ src/test/java/com/ahc/tspetclinic/TsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahc.tspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
