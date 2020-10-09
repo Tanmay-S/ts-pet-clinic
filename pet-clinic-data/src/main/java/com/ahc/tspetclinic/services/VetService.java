7c60563a932c858f7a93fc96842e4a94f724cf92
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-10-09 19:00:07 +0530

    Implement interface for Services. Closes #4

---
+++ pet-clinic-data/src/main/java/com/ahc/tspetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.ahc.tspetclinic.services;
+
+import com.ahc.tspetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
