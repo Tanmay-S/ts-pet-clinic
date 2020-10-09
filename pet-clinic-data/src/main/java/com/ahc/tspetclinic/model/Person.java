d1444b994baf783cd3db833fcdd8869ea59b2833
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-10-09 19:09:30 +0530

    Adding Base Entity. Closes #9

--- pet-clinic-data/src/main/java/com/ahc/tspetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/ahc/tspetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.ahc.tspetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/ahc/tspetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/ahc/tspetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.ahc.tspetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
