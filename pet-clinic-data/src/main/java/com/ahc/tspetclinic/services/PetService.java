0a4dc161156ec25b0930de1c84984d143a0a87c0
Tanmay Srivastava <52026136+Tanmay-S@users.noreply.github.com>
2020-10-14 18:12:22 +0530

    Refactoring to common Crud Interface. Closes #10

--- pet-clinic-data/src/main/java/com/ahc/tspetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/ahc/tspetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.ahc.tspetclinic.services;
 
 import com.ahc.tspetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/ahc/tspetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/ahc/tspetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.ahc.tspetclinic.services;
 
 import com.ahc.tspetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
